dab76f95b112f1c2a44e82a0229f4f56a86d027a
GrantRotzien <grantqrotzien@gmail.com>
2017-10-27 15:31:05 -0700

    add backend js logic, not yet functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
+function numLister(num) {
+  var numArray = [];
+  for (var i = 1; i <= num; i++) {
+    if (i % 3 === 0) {
+      numArray.push(" " + "I'm sorry, Dave. I'm afraid I can't do that.");
+    } else {
+      numArray.push(" " + i);
+    }
+  }
+  //Case is support for specs
+  var beepCase = numArray.toString().replace(/1/g, " " + "Beep!");
+  var boopCase = beepCase.replace(/0/g, " " + "Boop!");
+  var outputNum = boopCase.replace(/,/g, "");
 
-
-var toInteger = function(stringArray) {
-  var parsedInteger = [];
-  stringArray.forEach(function(string) {
-    var integer = parseInt(string);
-    parsedInteger.push(integer);
-    console.log(parsedInteger)
-  });
-
+  return outputNum
+  console.log(outputNum);
 };
 
 
@@ -26,7 +32,7 @@ $(function() {
   $("#counter").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#num-input").val());
-    var output = inputCounter(userInput);
+    var output = numLister(userInput);
     $("#countup-output").text(output);
   });
 });
